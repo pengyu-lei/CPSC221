f63b180c777adbb3d35dd4eb350690847ef72b3e
Aaron Lei <115329606+pengyu-lei@users.noreply.github.com>
2023-10-19 20:41:55 -0700

    add lab_trees

---
+++ Labs/lab_trees/.objs/main.d
@@ -0,0 +1,12 @@
+.objs/main.o: main.cpp binarytree.h binarytree_given.cpp printtree.h \
+  binarytree.cpp coloredout.h
+
+binarytree.h:
+
+binarytree_given.cpp:
+
+printtree.h:
+
+binarytree.cpp:
+
+coloredout.h:
---
+++ Labs/lab_trees/.objs/test.d
@@ -0,0 +1,12 @@
+.objs/test.o: test.cpp catch.hpp binarytree.h binarytree_given.cpp \
+  printtree.h binarytree.cpp
+
+catch.hpp:
+
+binarytree.h:
+
+binarytree_given.cpp:
+
+printtree.h:
+
+binarytree.cpp:
