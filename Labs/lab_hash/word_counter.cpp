4ec2d4773f054a257bfd30e3d96e7b68afa74541
Aaron Lei <115329606+pengyu-lei@users.noreply.github.com>
2023-11-12 00:56:23 -0800

    finish lab_hash

---
+++ Labs/lab_hash/word_counter.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file word_counter.cpp
+ * Implementation of the WordFreq class.
+ *
+ * @author Chase Geigle
+ * @date Spring 2011
+ * @date Spring 2012
+ */
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::istringstream;
+using std::pair;
+using std::string;
+using std::vector;
+
+template <template <class K, class V> class Dict>
+WordFreq<Dict>::WordFreq(const string& filename) : dict(256) {
+    /**
+     * @todo Implement this function.
+     * @see char_counter.cpp if you're having trouble.
+     */
+    TextFile infile(filename);
+
+    while (infile.good()) {
+        dict[infile.getNextWord()]++;
+    }
+}
+
+template <template <class K, class V> class Dict>
+vector<pair<string, int>> WordFreq<Dict>::getWords(int threshold) {
+    vector<pair<string, int>> ret;
+    /**
+     * @todo Implement this function.
+     * @see char_counter.cpp if you're having trouble.
+     */
+
+    for (const auto& kv : dict) {
+        if (kv.second >= threshold) ret.push_back(kv);
+    }
+    //(void)threshold;  // prevent warnings... When you implement this function, remove this line.
+
+    return ret;
+}
